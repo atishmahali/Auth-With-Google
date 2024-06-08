47e5990fdfd757ccf71737c3d4a7ede404fb3c11
Atish Ranjan Mahali <70005329+atishmahali@users.noreply.github.com>
2024-06-08 12:45:14 +0530

    Create firebaseConfig.js

---
+++ src/googleSignin/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAKHq9UbymKdp9l7iPexrmk6O0VXYKJdqg",
+    authDomain: "xeno-333.firebaseapp.com",
+    projectId: "xeno-333",
+    storageBucket: "xeno-333.appspot.com",
+    messagingSenderId: "954757659329",
+    appId: "1:954757659329:web:a1ff0c3e1620c67a15f240",
+    measurementId: "G-RQFPQSNB4Q"
+  };
+export default firebaseConfig;
